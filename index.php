2efda490e4677d5e7f32f42f80a7b9f9cf5c96de
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-02-05 12:36:33 +0100

    changes to btns

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
     <p class="header__content-description ">
       كتب تعليمية قابلة لإعادة الاستخدام لمساعدة طفلك على تكوين الرسائل بشكل مثالي من خلال اتباع الخطوط المرسومة مسبقًا للحصول على الإيماءة الصحيحة لكل حرف.
     </p>
-    <a class="btn btn--tertiary header__btn" href="#buy-form">أطلب الأن</a>
+    <a class="btn btn--attract header__btn" href="#buy-form">أطلب الأن</a>
   </div>
 </header>
 <section class="information">
@@ -130,7 +130,7 @@
   </div>
 </section>
 <div class="buy-now-sticky">
-  <a href="#buy-form" class="btn btn--tertiary button-sticky b-center" style="margin: 0 auto;">شراء الآن</a>
+  <a href="#buy-form" class="btn btn--attract button-sticky b-center" style="margin: 0 auto;">شراء الآن</a>
   <a href="https://wa.me/212620226054?text=%D8%A3%D8%B1%D9%8A%D8%AF%20%D8%B4%D8%B1%D8%A7%D8%A1%20%D9%85%D8%A8%D8%B1%D8%AF%20%D8%A7%D9%84%D8%AC%D9%88%20%D8%A7%D9%84%D9%85%D8%AD%D9%85%D9%88%D9%84" class="button-whatsapp button-sticky  hidden"><img src="dist/images/icons8-whatsapp-64.png" class="button-whatsapp__img" /> الواتساب</a>
 </div>
 <div class="<?php
